582460b1fd5be75266408c7e41a706bf53577de8
Ian Calica <Iggc654@gmail.com>
2025-11-10 20:50:24 +0800

    Updated axios config

--- src/utils/client.jsx
+++ src/utils/client.jsx
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const getBaseURL = () => {
   if (import.meta.env.DEV) {
-    return "http://localhost:8000"; // Development
+    return "http://localhost:8000/api"; // Development
   } else {
-    return "https://fileguardpython-backend.onrender.com"; // Production
+    return "https://fileguardpython-backend.onrender.com/api"; // Production
   }
 };
 
@@ -13,30 +13,6 @@ const client = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
-});
-
-// 🔹 Automatically attach Supabase token to every request
-client.interceptors.request.use(
-  (config) => {
-    // Try to get the Supabase session from localStorage
-    const session = JSON.parse(localStorage.getItem("supabase_session"));
-    const token = session?.access_token;
-
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
-
-// 🔹 Handle multipart/form-data automatically when uploading files
-client.interceptors.request.use((config) => {
-  if (config.data instanceof FormData) {
-    config.headers["Content-Type"] = "multipart/form-data";
-  }
-  return config;
 });
 
 export default client;
